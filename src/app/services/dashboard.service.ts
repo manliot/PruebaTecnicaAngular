de9820f61c7591295177b4b7ae58b5feaffa792b
manliot <manliot@uninorte.edu.co>
2023-03-08 02:56:48 -0500

    feat: read csv

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { DashboardItem } from "../interfaces/dashboard.item.type";
+//import { DashboardItem } from "../interfaces/dashboard.item.type";
+import { parse } from 'papaparse';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,92 +13,37 @@ export class DashboardService {
   constructor(private http: HttpClient) {
   }
 
-  public getNewDashboardData(): DashboardItem[] {
-    return [
-      {
-        campaignTitle: "Campaña 1",
-        campaignType: "Promoción",
-        impactedUsers: 10,
-        creationDate: new Date(),
-        color: "#9469AD",
-        sendDate: new Date(),
-        status: "Creada",
-        theme: "Salud",
-        themeIcon: "Bullyng@PYPGrandes",
-        expandableData: [
-          {
-            icon: "Sms",
-            value: "10",
-            label: "SMS Enviados"
-          },
-          {
-            icon: "notifications",
-            value: "12",
-            label: "N. Push Enviadas"
-          },
-          {
-            icon: "LocalPhone",
-            value: "14",
-            label: "Llamadas Enviadas"
-          }
-        ]
-      },
-      {
-        campaignTitle: "Campaña 3",
-        campaignType: "Promoción",
-        impactedUsers: 10,
-        creationDate: new Date(),
-        sendDate: new Date(),
-        status: "Creada",
-        theme: "Salud",
-        themeIcon: "Covid@PYPGrandes",
-        color: "#419AFF",
-        expandableData: [
-          {
-            icon: "Sms",
-            value: "10",
-            label: "SMS Enviados"
-          },
-          {
-            icon: "notifications",
-            value: "12",
-            label: "N. Push Enviadas"
-          },
-          {
-            icon: "LocalPhone",
-            value: "14",
-            label: "Llamadas Enviadas"
-          }
-        ]
-      },
-      {
-        campaignTitle: "Campaña 1",
-        campaignType: "Promoción",
-        impactedUsers: 10,
-        creationDate: new Date(),
-        sendDate: new Date(),
-        status: "Creada",
-        theme: "Salud",
-        color: "#65BE51",
-        themeIcon: "nutricion@PYPGrandes",
-        expandableData: [
-          {
-            icon: "Sms",
-            value: "10",
-            label: "SMS Enviados"
-          },
-          {
-            icon: "notifications",
-            value: "12",
-            label: "N. Push Enviadas"
-          },
-          {
-            icon: "LocalPhone",
-            value: "14",
-            label: "Llamadas Enviadas"
-          }
-        ]
-      }
-    ];
+  public async getNewDashboardData() {
+    const filePath = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQLMujrcrLDFPS_Qia-SWNJAAVdiic2ZgX3QfzgoN_hQOuSrfm-5qfdCwuLD6OaUlMgZGRrHPCQJR8w/pub?gid=1362881472&single=true&output=csv'
+    //const filePath = 'time_series_covid19_deaths_US.csv'
+
+    const dataParsed = new Promise((resolve,reject)=>{
+      parse(filePath, {
+        header: true,
+        delimiter: ",",
+        download: true,
+        endoding: 'utf8',
+        complete: (results) => {
+            const data = results.data.map((row)=>{
+            return {
+              'uid': row.UID,
+              'country': row.iso,
+              'provinceState': row.Province_State,
+              'city': row.Admin2,
+              'd_2021_04_24': parseInt(row['4/24/21']),
+              'd_2021_04_25': parseInt(row['4/25/21']),
+              'd_2021_04_26': parseInt(row['4/26/21'])
+            }
+          })
+          resolve(data)
+        },
+        error: (err) =>{
+          reject(err)
+        }
+      })
+    })
+
+    const data_res = await dataParsed
+    return data_res  
   }
 }
